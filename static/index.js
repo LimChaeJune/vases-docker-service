3bf80db9220c02d92d8c432f3be4fb2b136dfc1c
석환 김 <sh2.kim@saigeresearch.ai>
2024-01-25 12:43:41 +0900

    update pure login js

--- static/index.js
+++ static/index.js
@@ -1,10 +1,12 @@
-const signUpBtn = document.getElementById("signUp");
-const signInBtn = document.getElementById("signIn");
-const container = document.querySelector(".container");
+document.onload = function () {
+  const signUpBtn = document.getElementById("signUp");
+  const signInBtn = document.getElementById("signIn");
+  const container = document.querySelector(".container");
 
-signUpBtn.addEventListener("click", () => {
-  container.classList.add("right-panel-active");
-});
-signInBtn.addEventListener("click", () => {
-  container.classList.remove("right-panel-active");
-});
+  signUpBtn.addEventListener("click", () => {
+    container.classList.add("right-panel-active");
+  });
+  signInBtn.addEventListener("click", () => {
+    container.classList.remove("right-panel-active");
+  });
+};
