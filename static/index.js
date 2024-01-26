97fbe454d6750246a3ba4df85bb63439ee095ca5
sox-dev <soxuser1@gmail.com>
2024-01-26 19:55:01 +0900

    test commit

--- static/index.js
+++ static/index.js
@@ -1,14 +1,13 @@
-
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
   const signUpBtn = document.getElementById("signUp");
   const signInBtn = document.getElementById("signIn");
   const container = document.querySelector(".container");
 
   signUpBtn.addEventListener("click", () => {
+    console.log("test");
     container.classList.add("right-panel-active");
   });
   signInBtn.addEventListener("click", () => {
     container.classList.remove("right-panel-active");
   });
 });
- 
